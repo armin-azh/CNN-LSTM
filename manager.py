d5229313d6686e3c07f4e2a50fd0eadcc1a4e5a0
armin azhdehnia <arminmk18@gmail.com>
2021-09-30 02:10:51 -0400

    add options

--- manager.py
+++ manager.py
@@ -7,6 +7,22 @@ def main(arguments: Namespace):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+    parser.add_argument("--train", help="enable training process", action="store_true")
+    parser.add_argument("--validation", help="enable validation process", action="store_true")
+    parser.add_argument("--input", dest="in_file", help="input file to train or validation", type=str, required=True)
+
+    # hyper parameter
+    parser.add_argument("--conv_filters", help="number of convolution filters", type=int, default=32)
+    parser.add_argument("--conv_kernel", help="convolution kernel size dimension", type=int, default=1)
+    parser.add_argument("--conv_padding", help="convolution padding type", type=str, default="same",
+                        choices=["valid", "same"])
+    parser.add_argument("--pool_size", help="pool size", type=str, default=1)
+    parser.add_argument("--pool_padding", help="pool padding type", type=str, default="same", choices=["valid", "same"])
+    parser.add_argument("--lstm_hid", help="number of lstm hidden unit", type=int, default=64)
+    parser.add_argument("-time_step", help="number of time step", type=int, default=10)
+    parser.add_argument("--batch_size", help="determine the batch size", type=int, default=64)
+    parser.add_argument("--lr", help="learning rate", type=float, default=1e-3)
+
     args = parser.parse_args()
 
     main(arguments=args)
--- settings.py
+++ settings.py
@@ -6,3 +6,8 @@ import torch
 project_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 
 has_cuda = torch.cuda.is_available()
+
+selected_column = []
+
+output_dir = project_dir.joinpath("result")
+output_dir.mkdir(parents=True, exist_ok=True)
